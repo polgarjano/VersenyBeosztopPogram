4ba0755b90f33438f7eb44c4a054566e762ed504
polgar janos <polgarjano@gmail.com>
2022-08-03 20:20:58 +0200

    Mi assigner (not tested)

--- src/main/java/hu/unideb/inf/szakdolgozat/model/assigner/MiAssigner.java
+++ src/main/java/hu/unideb/inf/szakdolgozat/model/assigner/MiAssigner.java
@@ -1,25 +1,91 @@
 package hu.unideb.inf.szakdolgozat.model.assigner;
 
-import hu.unideb.inf.szakdolgozat.model.dto.Competition;
-import hu.unideb.inf.szakdolgozat.model.dto.Competitor;
-import hu.unideb.inf.szakdolgozat.model.dto.EventType;
-import hu.unideb.inf.szakdolgozat.model.dto.Schedule;
+import hu.unideb.inf.szakdolgozat.model.dto.*;
+import lombok.Data;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class MiAssigner {
 
     private List<List<CompetitionEvent>> choseAbelGroups = new LinkedList<>();
 
+    private List<List<EventType>> ScheduleByEvent;
+
+    private EventNode root = new EventNode(null, null, 0, 0);
+    private LinkedList<EventNode> perem = new LinkedList<>();
 
     public Schedule creatStartList(Competition competition) {
         creatChoseAbleGroups(competition);
+        for (int i = 0; i < choseAbelGroups.get(0).size(); i++) {
+
+            perem.add(root.add(choseAbelGroups.get(0).get(i).eventType, 1, 0));
+
+        }
+        creatTree();
 
 
+        return null;
+    }
+
+    private void creatTree() {
+       while (perem.size()!=0){
+            EventNode eventNode = perem.poll();
+            Set<EventType> scheduledEvents = new HashSet<>();
+            scheduledEvents.add(eventNode.eventType);
+            EventNode actualParent = eventNode.parent;
+            while (actualParent.eventType != null) {
+                scheduledEvents.add(actualParent.eventType);
+                actualParent = actualParent.parent;
+            }
+            boolean nextEventIsPistol = nextEventIsPistol(scheduledEvents, eventNode.eventType.isIsPistolEvent());
+
+            int groupPosition = findGroup(eventNode.eventType.getEventGroup());
+
+            if (nextEventIsPistol != eventNode.eventType.isIsPistolEvent()) {
+                groupPosition = 0;
+            }
+                for (int i = groupPosition; i < choseAbelGroups.size(); i++) {
+                    boolean added = false;
+                    for (CompetitionEvent event :
+                            choseAbelGroups.get(i)) {
+                        if (event.eventType.isIsPistolEvent() == nextEventIsPistol && !scheduledEvents.contains(event.eventType)) {
+                            perem.addLast(eventNode.add(event.eventType, 1, 0));
+                            added = true;
+                        }
+                    }
+                    if(added){
+                        break;
+                    }
+                }
+
+        }
+    }
+
+    private boolean nextEventIsPistol(Set<EventType> scheduledEvents, boolean isPistolEvent) {
+
+        if (scheduledEvents.stream().allMatch(x -> x.isIsPistolEvent() == isPistolEvent)) {
+
+            for (List<CompetitionEvent> events : choseAbelGroups) {
+                for (CompetitionEvent event : events) {
+                    if (!scheduledEvents.contains(event) && isPistolEvent == event.eventType.isIsPistolEvent()) {
+                        return isPistolEvent;
+                    }
+                }
+            }
+            return !isPistolEvent;
+        } else {
+            return isPistolEvent;
+        }
+
+    }
 
 
-        return  null;
+    private int findGroup(int eventGroup) {
+        for (int i = 0; i < choseAbelGroups.size(); i++) {
+            if (choseAbelGroups.get(i).get(0).eventType.getEventGroup() == eventGroup) ;
+            return i;
+        }
+        return -1;
     }
 
     private void creatChoseAbleGroups(Competition competition) {
@@ -33,18 +99,18 @@ public class MiAssigner {
         int actualGroupNumber = eventTypes.get(0).getEventGroup();
         choseAbelGroups.add(new LinkedList<>());
         var actualGroup = choseAbelGroups.get(0);
-        for(int i = 0; i < eventTypes.size(); i++){
-            if(actualGroupNumber != eventTypes.get(i).getEventGroup()){
+        for (int i = 0; i < eventTypes.size(); i++) {
+            if (actualGroupNumber != eventTypes.get(i).getEventGroup()) {
                 actualGroupNumber = eventTypes.get(i).getEventGroup();
                 choseAbelGroups.add(new LinkedList<>());
-                actualGroup = choseAbelGroups.get(choseAbelGroups.size()-1);
+                actualGroup = choseAbelGroups.get(choseAbelGroups.size() - 1);
             }
             int finalI = i;
             Long numberOfCompetitors = competition.getCompetitors()
                     .stream()
                     .filter(x -> x.getEventType() == eventTypes.get(finalI))
                     .count();
-            CompetitionEvent competitionEvent = new CompetitionEvent(eventTypes.get(i),numberOfCompetitors,numberOfCompetitors);
+            CompetitionEvent competitionEvent = new CompetitionEvent(eventTypes.get(i), numberOfCompetitors, numberOfCompetitors);
             actualGroup.add(competitionEvent);
 
         }
@@ -63,4 +129,32 @@ public class MiAssigner {
             this.numberOfCompetitors = numberOfCompetitors;
         }
     }
+
+
+    private class EventNode {
+        EventNode parent;
+
+        EventType eventType;
+
+        int numberOfRelay;
+
+        int freeLaneAfterThisNode;
+
+        ArrayList<EventNode> childrenNodes;
+
+        EventNode add(EventType eventType, int numberOfRelay, int usedLanesInThisNode) {
+            var newNode = new EventNode(this, eventType, numberOfRelay, usedLanesInThisNode);
+            childrenNodes.add(newNode);
+            return newNode;
+        }
+
+        public EventNode(EventNode parent, EventType eventType, int numberOfRelay, int freeLaneAfterThisNode) {
+            this.parent = parent;
+            this.eventType = eventType;
+            this.numberOfRelay = numberOfRelay;
+            this.freeLaneAfterThisNode = freeLaneAfterThisNode;
+            childrenNodes = new ArrayList<>();
+        }
+    }
+
 }
